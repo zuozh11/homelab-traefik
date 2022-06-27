d2ce8bd6f8a34f8bd15d1164f4c4fd3e60febd2a
zuozhi <zuozhi22@outlook.com>
2022-06-27 22:38:49 +0800

    feat: 编写初始化脚本

---
+++ init.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+echo '仅用于初始化,初始化完成后自动启动容器'
+echo
+echo '开始初始化...'
+echo '启动容器...'
+docker-compose up -d
+echo '切换到Let`s letsencrypt'
+docker exec acme.sh --set-default-ca --server letsencrypt
+echo '设置更新'
+docker exec acme.sh --upgrade --auto-upgrade
+echo '生成证书'
+docker exec acme.sh --issue --dns dns_dp -d '*.kamipon.com'
+echo '安装证书'
+docker exec acme.sh --install-cert -d '*.kamipon.com' \
+--key-file       /data/cert/kamipon.key  \
+--fullchain-file /data/cert/kamipon.crt \
+--reloadcmd     "echo '证书安装完成'"
+echo '初始化完毕!'
